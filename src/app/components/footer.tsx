ade68975f991f48c548796d5137120c95fb610d4
Anas K <anaskhan.ak2005@gmail.com>
2025-04-12 20:25:30 +0500

    Changes to Footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,47 +1,63 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Footer() {
+	const [hover, setHover] = useState(false);
+
+	const toggleHover = () => setHover((prev) => !prev);
+
 	return (
-		<div>
-			<div>
-				<div className=" border-t border-t-gray-400">
-					<div className="flex flex-col items-center justify-center py-12 bg-white">
-						<div>
-							<div className="text-4xl font-bold py-6 text-neutral-900  capitalize">
-								Creative Pluto technologies
-							</div>
-						</div>
-						<div className="flex gap-5 items-center justify-center py-6">
-							<a>
-								<Image
-									width={24}
-									height={24}
-									src="/whatsapp.svg"
-									alt=""
-									className="w-6 h-6"
-								/>
-							</a>
-							<a>
-								<Image
-									width={24}
-									height={24}
-									src="/facebook.svg"
-									alt=""
-									className="w-6 h-6"
-								/>
-							</a>
-							<a>
-								<Image
-									width={24}
-									height={24}
-									src="/instagram.svg"
-									alt=""
-									className="w-6 h-6"
-								/>
-							</a>
-						</div>
-					</div>
-				</div>
+		<footer className="border-t border-gray-400 bg-white">
+			<div className="flex flex-col items-center justify-center py-12">
+				<h1 className="text-4xl font-bold py-6 text-neutral-900 capitalize">
+					Creative Pluto Technologies
+				</h1>
+
+				<nav className="flex gap-5 items-center justify-center py-6">
+					{/* WhatsApp */}
+					<Link
+						href="https://api.whatsapp.com/send/?phone=13656491676&text&type=phone_number&app_absent=0"
+						passHref
+					>
+						<Image
+							width={32}
+							height={32}
+							src="/logo/whatsapp-col.svg"
+							alt="WhatsApp Logo"
+							className="w-8 h-8 transition-all duration-500 grayscale hover:grayscale-0"
+						/>
+					</Link>
+
+					{/* LinkedIn */}
+					<Link
+						href="https://www.linkedin.com/company/creative-pluto-technologies/"
+						passHref
+					>
+						<Image
+							width={32}
+							height={32}
+							src="/logo/linkedin.svg"
+							alt="LinkedIn Logo"
+							className="w-8 h-8 transition-all duration-500 grayscale hover:grayscale-0"
+						/>
+					</Link>
+
+					{/* Twitter */}
+					<Link href="https://x.com/CreativePluto" passHref>
+						<Image
+							width={32}
+							height={32}
+							src={hover ? "/logo/twitter.svg" : "/logo/twitter-light.svg"}
+							alt="Twitter Logo"
+							className="w-8 h-8 transition-all duration-500 hover:ease-in-out"
+							onMouseEnter={toggleHover}
+							onMouseLeave={toggleHover}
+						/>
+					</Link>
+				</nav>
 			</div>
-		</div>
+		</footer>
 	);
 }
